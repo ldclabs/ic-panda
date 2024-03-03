45cc2e50459f1bd0f448d01a923f12c979cdc0d3
0xZensh <txr1883@gmail.com>
2024-03-03 23:15:56 +0800

    Implement luckydraw

--- src/ic_panda_luckypool/src/lib.rs
+++ src/ic_panda_luckypool/src/lib.rs
@@ -1,5 +1,6 @@
-use candid::{candid_method, types::number::Nat, Principal};
+use candid::{candid_method, Nat, Principal};
 use ic_captcha::CaptchaBuilder;
+use num_traits::cast::ToPrimitive;
 use once_cell::sync::Lazy;
 use std::{
     convert::{From, Into},
@@ -9,6 +10,7 @@ use std::{
 use icrc_ledger_types::{
     icrc1::account::Account,
     icrc1::transfer::{TransferArg, TransferError},
+    icrc2::transfer_from::{TransferFromArgs, TransferFromError},
 };
 
 mod crypto;
@@ -16,126 +18,212 @@ mod store;
 mod types;
 
 use crypto::mac_256;
-use store::{
-    active_user, deactive_user, get_airdrop, set_airdrop, set_captcha_secret, with_captcha_secret,
-};
-use types::{AirdropClaimInput, CaptchaOutput, Challenge};
 
 const SECOND: u64 = 1_000_000_000;
-const CAPTCHA_EXPIRE: u64 = SECOND * 60 * 5;
+const CAPTCHA_EXPIRE_SEC: u64 = 60 * 5;
 
+const TRANS_FEE: u64 = 10_000;
 const TOKEN_1: u64 = 100_000_000;
+const ICP_1: u64 = ic_ledger_types::Tokens::SUBDIVIDABLE_BY;
 const AIRDROP_AMOUNT: u64 = TOKEN_1 * 10;
+const LOWEST_LUCKYDRAW_BALANCE: u64 = TOKEN_1 * 100;
+
+// 344693032001 from b"PANDA"
+const LUCKYDRAW_DIVISOR: u64 = u64::from_be_bytes([0, 0, 0, b'P', b'A', b'N', b'D', b'A']);
 
 static ANONYMOUS: Principal = Principal::anonymous();
+static ICP_CANISTER: Principal = ic_ledger_types::MAINNET_LEDGER_CANISTER_ID;
 static TOKEN_CANISTER: Lazy<Principal> =
-    Lazy::new(|| Principal::from_text("bkyz2-fmaaa-aaaaa-qaaaq-cai").expect("invalid principal"));
+    Lazy::new(|| Principal::from_text("bkyz2-fmaaa-aaaaa-qaaaq-cai").expect("invalid principal")); // TODO: update token canister id
+static DAO_CANISTER: Lazy<Principal> =
+    Lazy::new(|| Principal::from_text("a7cug-2qaaa-aaaap-ab3la-cai").expect("invalid principal")); // TODO: update dao canister id
 static CAPTCHA_BUILDER: Lazy<CaptchaBuilder> = Lazy::new(CaptchaBuilder::new);
 
 #[ic_cdk::init]
 fn init() {
-    ic_cdk_timers::set_timer(Duration::from_millis(1), || {
+    ic_cdk_timers::set_timer(Duration::from_nanos(0), || {
         ic_cdk::spawn(load_captcha_secret())
     });
 }
 
 #[ic_cdk::post_upgrade]
 fn post_upgrade() {
-    ic_cdk_timers::set_timer(Duration::from_millis(1), || {
+    ic_cdk_timers::set_timer(Duration::from_nanos(0), || {
         ic_cdk::spawn(load_captcha_secret())
     });
 }
 
-async fn load_captcha_secret() {
-    // can't be used in `init` and `post_upgrade`
-    let rr = ic_cdk::api::management_canister::main::raw_rand()
-        .await
-        .expect("failed to get random bytes");
-
-    set_captcha_secret(mac_256(&rr.0, b"Captcha Secret"));
-    ic_cdk::api::print("Captcha secret loaded\n");
-}
-
 #[ic_cdk::query]
-#[candid_method(query)]
+#[candid_method]
 fn debug() -> String {
     "OK".to_string()
 }
 
 #[ic_cdk::query]
-#[candid_method(query)]
-fn whoami() -> Principal {
-    ic_cdk::caller()
+#[candid_method]
+fn whoami() -> Result<Principal, ()> {
+    Ok(ic_cdk::caller())
 }
 
 #[ic_cdk::query]
-#[candid_method(query)]
-async fn airdrop() -> Result<Option<Nat>, ()> {
-    let user = ic_cdk::caller();
-    if user == ANONYMOUS {
-        return Ok(None);
-    }
-
-    let res = get_airdrop(user);
-    Ok(res.map(Nat::from))
+#[candid_method]
+fn state() -> Result<store::State, ()> {
+    Ok(store::state::get())
 }
 
 #[ic_cdk::query(composite = true)]
-#[candid_method(query)]
+#[candid_method]
 async fn lucky_pool_balance() -> Result<Nat, String> {
     token_balance_of(ic_cdk::id()).await
 }
 
 #[ic_cdk::update(guard = "is_authenticated")]
-#[candid_method(update)]
-async fn captcha() -> Result<CaptchaOutput, String> {
+#[candid_method]
+async fn captcha() -> Result<types::CaptchaOutput, String> {
     let rr = ic_cdk::api::management_canister::main::raw_rand()
         .await
         .map_err(|_err| "failed to get random bytes".to_string())?;
 
     let captcha = CAPTCHA_BUILDER.generate(&rr.0, None);
     let now = ic_cdk::api::time();
-    let challenge = Challenge {
+    let challenge = types::Challenge {
         code: captcha.text().to_lowercase(),
         time: now / SECOND,
     };
 
-    let challenge = with_captcha_secret(|secret| challenge.sign_to_base64(secret))?;
-    Ok(CaptchaOutput {
+    let challenge = store::captcha::with_secret(|secret| challenge.sign_to_base64(secret))?;
+    Ok(types::CaptchaOutput {
         img_base64: captcha.to_base64(0),
         challenge,
     })
 }
 
+#[ic_cdk::query]
+#[candid_method]
+async fn airdrop_status() -> Result<bool, ()> {
+    let user = ic_cdk::caller();
+    if user == ANONYMOUS {
+        return Ok(false);
+    }
+
+    Ok(store::airdrop::has(user))
+}
+
+#[ic_cdk::query]
+#[candid_method]
+async fn airdrop_total() -> Result<u64, ()> {
+    Ok(store::airdrop::total())
+}
+
+#[ic_cdk::query]
+#[candid_method]
+async fn luckydraw_total() -> Result<u64, ()> {
+    Ok(store::luckydraw::total())
+}
+
+#[ic_cdk::query]
+#[candid_method]
+async fn airdrop_logs(args: types::LogsInput) -> Result<types::LogsOutput<store::AirdropLog>, ()> {
+    let res = store::airdrop::logs(10, args.index);
+    Ok(types::LogsOutput {
+        next_index: res.1,
+        logs: res.0,
+    })
+}
+
+#[ic_cdk::query]
+#[candid_method]
+async fn luckydraw_logs(
+    args: types::LogsInput,
+) -> Result<types::LogsOutput<store::LuckyDrawLog>, ()> {
+    let res = store::luckydraw::logs(10, args.index);
+    Ok(types::LogsOutput {
+        next_index: res.1,
+        logs: res.0,
+    })
+}
+
 #[ic_cdk::update(guard = "is_authenticated")]
-#[candid_method(update)]
-async fn claim_airdrop(args: AirdropClaimInput) -> Result<Nat, String> {
-    let expire_at = ic_cdk::api::time() - CAPTCHA_EXPIRE;
-    let _ = with_captcha_secret(|secret| {
-        Challenge::verify_from_base64(
+#[candid_method]
+async fn claim_airdrop(args: types::AirdropClaimInput) -> Result<Nat, String> {
+    let now = ic_cdk::api::time() / SECOND;
+    let expire_at = now - CAPTCHA_EXPIRE_SEC;
+    let _ = store::captcha::with_secret(|secret| {
+        types::Challenge::verify_from_base64(
             secret,
             &args.code.to_lowercase(),
             &args.challenge,
-            expire_at / SECOND,
+            expire_at,
         )
     })?;
 
     let user = ic_cdk::caller();
-    if let Some(n) = get_airdrop(user) {
-        return Ok(Nat::from(n));
+    if store::airdrop::has(user) {
+        return Ok(Nat::from(AIRDROP_AMOUNT));
     }
 
-    if !active_user(user) {
+    if store::airdrop::balance() < AIRDROP_AMOUNT {
+        return Err("airdrop pool is empty".to_string());
+    }
+
+    if !store::user::active(user) {
         return Err("try again later".to_string());
     }
 
     let _guard = scopeguard::guard((), |_| {
-        deactive_user(user);
+        store::user::deactive(user);
     });
 
-    let _block_idx = token_transfer_to(user, AIRDROP_AMOUNT).await?;
-    set_airdrop(user, AIRDROP_AMOUNT);
+    // don't need to check balance, because the token canister will reject the transfer if the balance is insufficient
+    let _block_idx = token_transfer_to(user, Nat::from(AIRDROP_AMOUNT)).await?;
+    let _res = store::airdrop::update(user, now, AIRDROP_AMOUNT);
+    // TODO: trace error logs
+    ic_cdk::api::print(format!("claim_airdrop: {:?}\n", _res));
+
     Ok(Nat::from(AIRDROP_AMOUNT))
+}
+
+#[ic_cdk::update(guard = "is_authenticated")]
+#[candid_method]
+async fn luckydraw(args: types::LuckyDrawInput) -> Result<Nat, String> {
+    if args.icp < 1 || args.icp > 10 {
+        return Err("invalid icp amount".to_string());
+    }
+
+    let user = ic_cdk::caller();
+    if !store::user::active(user) {
+        return Err("try again later".to_string());
+    }
+    let _guard = scopeguard::guard((), |_| {
+        store::user::deactive(user);
+    });
+
+    let balance = token_balance_of(ic_cdk::id()).await?;
+    if balance < LOWEST_LUCKYDRAW_BALANCE * args.icp as u64 + TRANS_FEE {
+        return Err("insufficient token balance for luckydraw".to_string());
+    }
+
+    let now = ic_cdk::api::time() / SECOND;
+    let rr = ic_cdk::api::management_canister::main::raw_rand()
+        .await
+        .map_err(|_err| "failed to get random bytes".to_string())?;
+    let icp = args.icp as u64 * ICP_1;
+    let (x, amount) = luckydraw_amount(&rr.0);
+
+    let _ = icp_transfer_from(user, Nat::from(icp)).await?;
+    let balance = token_balance_of(ic_cdk::id()).await?;
+    let balance: u64 = balance.0.to_u64().unwrap_or(0).saturating_sub(TRANS_FEE);
+    let amount = args.icp as u64 * amount;
+    let amount = if balance < amount { balance } else { amount };
+    if amount > 0 {
+        let _ = token_transfer_to(user, Nat::from(amount)).await?;
+    }
+
+    let _res = store::luckydraw::update(user, now, amount, icp, x);
+    // TODO: trace error logs
+    ic_cdk::api::print(format!("claim_airdrop: {:?}\n", _res));
+
+    Ok(Nat::from(amount))
 }
 
 fn is_authenticated() -> Result<(), String> {
@@ -144,6 +232,29 @@ fn is_authenticated() -> Result<(), String> {
     } else {
         Ok(())
     }
+}
+
+fn luckydraw_amount(rnd32: &[u8]) -> (u64, u64) {
+    let mut bytes = [0u8; 8];
+    bytes.copy_from_slice(&rnd32[0..8]);
+    let x = u64::from_be_bytes(bytes);
+    let a = x % LUCKYDRAW_DIVISOR;
+    let amount = match a / TOKEN_1 {
+        v if v <= 5 => 100000 * TOKEN_1,
+        v if v <= 1000 => 1000 * TOKEN_1,
+        v => v * TOKEN_1,
+    };
+    (x, amount)
+}
+
+async fn load_captcha_secret() {
+    // can't be used in `init` and `post_upgrade`
+    let rr = ic_cdk::api::management_canister::main::raw_rand()
+        .await
+        .expect("failed to get random bytes");
+
+    store::captcha::set_secret(mac_256(&rr.0, b"Captcha Secret"));
+    ic_cdk::api::print("Captcha secret loaded\n");
 }
 
 async fn token_balance_of(user: Principal) -> Result<Nat, String> {
@@ -156,11 +267,11 @@ async fn token_balance_of(user: Principal) -> Result<Nat, String> {
         },),
     )
     .await
-    .map_err(|err| format!("failed to get balance, error {:?}", err))?;
+    .map_err(|err| format!("failed to get balance, error: {:?}", err))?;
     Ok(balance)
 }
 
-async fn token_transfer_to(user: Principal, amount: u64) -> Result<Nat, String> {
+async fn token_transfer_to(user: Principal, amount: Nat) -> Result<Nat, String> {
     let (res,): (Result<Nat, TransferError>,) = ic_cdk::call(
         *TOKEN_CANISTER,
         "icrc1_transfer",
@@ -173,12 +284,37 @@ async fn token_transfer_to(user: Principal, amount: u64) -> Result<Nat, String> 
             fee: None,
             created_at_time: None,
             memo: None,
-            amount: Nat::from(amount),
+            amount,
         },),
     )
     .await
-    .map_err(|err| format!("failed to call icrc1_transfer, error {:?}", err))?;
-    res.map_err(|err| format!("failed to transfer, error {:?}", err))
+    .map_err(|err| format!("failed to call icrc1_transfer, error: {:?}", err))?;
+    res.map_err(|err| format!("failed to transfer tokens, error: {:?}", err))
+}
+
+async fn icp_transfer_from(user: Principal, amount: Nat) -> Result<Nat, String> {
+    let (res,): (Result<Nat, TransferFromError>,) = ic_cdk::call(
+        ICP_CANISTER,
+        "icrc2_transfer_from",
+        (TransferFromArgs {
+            spender_subaccount: None,
+            from: Account {
+                owner: user,
+                subaccount: None,
+            },
+            to: Account {
+                owner: *DAO_CANISTER,
+                subaccount: None,
+            },
+            fee: None,
+            created_at_time: None,
+            memo: None,
+            amount,
+        },),
+    )
+    .await
+    .map_err(|err| format!("failed to call icrc2_transfer_from, error: {:?}", err))?;
+    res.map_err(|err| format!("failed to transfer ICP from user, error: {:?}", err))
 }
 
 ic_cdk::export_candid!();
--- src/ic_panda_luckypool/src/store.rs
+++ src/ic_panda_luckypool/src/store.rs
@@ -1,4 +1,4 @@
-use candid::Principal;
+use candid::{CandidType, Principal};
 use ic_stable_structures::{
     memory_manager::{MemoryId, MemoryManager, VirtualMemory},
     storable::Bound,
@@ -9,10 +9,13 @@ use std::{borrow::Cow, cell::RefCell, collections::BTreeSet};
 
 type Memory = VirtualMemory<DefaultMemoryImpl>;
 
-#[derive(Clone, Debug, Serialize, Deserialize, Default)]
+#[derive(Clone, Debug, Serialize, Deserialize, Default, CandidType)]
 pub struct State {
+    pub airdrop_balance: u64,
     pub total_airdrop: u64,
-    pub total_lucky_draw: u64,
+    pub total_luckydraw: u64,
+    pub total_luckydraw_icp: u64,
+    pub total_luckydraw_count: u64,
 }
 
 // NOTE: the default configuration is dysfunctional, but it's convenient to have
@@ -31,13 +34,48 @@ impl Storable for State {
     }
 }
 
+// AirdropLog format: [user, time, token_amount]
+#[derive(Clone, Debug, Serialize, Deserialize, CandidType)]
+pub struct AirdropLog(Principal, u64, u64);
+
+impl Storable for AirdropLog {
+    const BOUND: Bound = Bound::Unbounded;
+
+    fn to_bytes(&self) -> Cow<[u8]> {
+        let mut buf = vec![];
+        ciborium::ser::into_writer(self, &mut buf).expect("failed to encode airdrop log");
+        Cow::Owned(buf)
+    }
+
+    fn from_bytes(bytes: Cow<'_, [u8]>) -> Self {
+        ciborium::de::from_reader(&bytes[..]).expect("failed to decode airdrop log")
+    }
+}
+
+// AirdropLog format: [user, time, token_amount, icp_amount, random_number]
+#[derive(Clone, Debug, Serialize, Deserialize, CandidType)]
+pub struct LuckyDrawLog(Principal, u64, u64, u64, u64);
+
+impl Storable for LuckyDrawLog {
+    const BOUND: Bound = Bound::Unbounded;
+
+    fn to_bytes(&self) -> Cow<[u8]> {
+        let mut buf = vec![];
+        ciborium::ser::into_writer(self, &mut buf).expect("failed to encode luckydraw log");
+        Cow::Owned(buf)
+    }
+
+    fn from_bytes(bytes: Cow<'_, [u8]>) -> Self {
+        ciborium::de::from_reader(&bytes[..]).expect("failed to decode luckydraw log")
+    }
+}
+
 const STATE_MEMORY_ID: MemoryId = MemoryId::new(0);
 const AIRDROP_MEMORY_ID: MemoryId = MemoryId::new(1);
 const AIRDROP_LOG_INDEX_MEMORY_ID: MemoryId = MemoryId::new(2);
 const AIRDROP_LOG_DATA_MEMORY_ID: MemoryId = MemoryId::new(3);
-const LUCKY_DRAW_MEMORY_ID: MemoryId = MemoryId::new(4);
-const LUCKY_DRAW_LOG_INDEX_MEMORY_ID: MemoryId = MemoryId::new(5);
-const LUCKY_DRAW_LOG_DATA_MEMORY_ID: MemoryId = MemoryId::new(6);
+const LUCKYDRAW_LOG_INDEX_MEMORY_ID: MemoryId = MemoryId::new(4);
+const LUCKYDRAW_LOG_DATA_MEMORY_ID: MemoryId = MemoryId::new(5);
 
 thread_local! {
     static CAPTCHA_SECRET: RefCell<[u8; 32]> = RefCell::new([0; 32]);
@@ -54,71 +92,206 @@ thread_local! {
         ).expect("failed to init STATE store")
     );
 
-    static AIRDROP: RefCell<StableBTreeMap<Principal, u64, Memory>> = RefCell::new(
+    static AIRDROP: RefCell<StableBTreeMap<Principal, (), Memory>> = RefCell::new(
         StableBTreeMap::init(
             MEMORY_MANAGER.with_borrow(|m| m.get(AIRDROP_MEMORY_ID)),
         )
     );
 
-    static AIRDROP_LOGS: RefCell<StableLog<Vec<u8>, Memory, Memory>> = RefCell::new(
+    static AIRDROP_LOGS: RefCell<StableLog<AirdropLog, Memory, Memory>> = RefCell::new(
         StableLog::init(
             MEMORY_MANAGER.with_borrow(|m| m.get(AIRDROP_LOG_INDEX_MEMORY_ID)),
             MEMORY_MANAGER.with_borrow(|m| m.get(AIRDROP_LOG_DATA_MEMORY_ID)),
         ).expect("failed to init AIRDROP_LOGS store")
     );
 
-    static LUCKY_DRAW: RefCell<StableBTreeMap<Principal, (u32, u64), Memory>> = RefCell::new(
-        StableBTreeMap::init(
-            MEMORY_MANAGER.with_borrow(|m| m.get(LUCKY_DRAW_MEMORY_ID)),
-        )
-    );
-
-    static LUCKY_DRAW_LOGS: RefCell<StableLog<Vec<u8>, Memory, Memory>> = RefCell::new(
+    static LUCKYDRAW_LOGS: RefCell<StableLog<LuckyDrawLog, Memory, Memory>> = RefCell::new(
         StableLog::init(
-            MEMORY_MANAGER.with_borrow(|m| m.get(LUCKY_DRAW_LOG_INDEX_MEMORY_ID)),
-            MEMORY_MANAGER.with_borrow(|m| m.get(LUCKY_DRAW_LOG_DATA_MEMORY_ID)),
+            MEMORY_MANAGER.with_borrow(|m| m.get(LUCKYDRAW_LOG_INDEX_MEMORY_ID)),
+            MEMORY_MANAGER.with_borrow(|m| m.get(LUCKYDRAW_LOG_DATA_MEMORY_ID)),
         ).expect("failed to init LUCKY_DRAW_LOGS store")
     );
 }
 
-pub fn with_captcha_secret<R>(f: impl FnOnce(&[u8]) -> R) -> R {
-    CAPTCHA_SECRET.with(|r| f(r.borrow().as_slice()))
+pub mod captcha {
+    use super::*;
+
+    pub fn with_secret<R>(f: impl FnOnce(&[u8]) -> R) -> R {
+        CAPTCHA_SECRET.with(|r| f(r.borrow().as_slice()))
+    }
+
+    pub fn set_secret(secret: [u8; 32]) {
+        CAPTCHA_SECRET.with(|r| *r.borrow_mut() = secret);
+    }
 }
 
-pub fn set_captcha_secret(secret: [u8; 32]) {
-    CAPTCHA_SECRET.with(|r| *r.borrow_mut() = secret);
-}
+pub mod airdrop {
+    use super::*;
 
-// add a user to the active user set, return true if the user is not in the set
-pub fn active_user(user: Principal) -> bool {
-    ACTIVE_USERS.with(|r| r.borrow_mut().insert(user))
-}
+    pub fn total() -> u64 {
+        AIRDROP_LOGS.with(|r| r.borrow().len())
+    }
 
-pub fn deactive_user(user: Principal) {
-    ACTIVE_USERS.with(|r| r.borrow_mut().remove(&user));
-}
+    pub fn balance() -> u64 {
+        STATE.with(|r| r.borrow().get().airdrop_balance)
+    }
 
-/// A helper function to access the state.
-pub fn with_state<R>(f: impl FnOnce(&State) -> R) -> R {
-    STATE.with(|r| f(r.borrow().get()))
-}
+    // check if a user has claimed airdrop.
+    pub fn has(user: Principal) -> bool {
+        AIRDROP
+            .with(|r| r.borrow().get(&user))
+            .map(|_| true)
+            .unwrap_or(false)
+    }
 
-/// A helper function to change the state.
-pub fn with_state_mut(f: impl FnOnce(&mut State)) {
-    STATE
-        .with(|r| {
+    // update luckydraw state and append a log.
+    // return the log index or an error message when append failed.
+    pub fn update(user: Principal, time: u64, amount: u64) -> Result<u64, String> {
+        AIRDROP.with(|r| r.borrow_mut().insert(user, ()));
+        STATE.with(|r| {
             let mut borrowed = r.borrow_mut();
             let mut state = borrowed.get().clone();
-            f(&mut state);
-            borrowed.set(state)
+            state.airdrop_balance = state.airdrop_balance.saturating_sub(amount);
+            state.total_airdrop = state.total_airdrop.saturating_add(amount);
+            borrowed
+                .set(state)
+                .map_err(|err| format!("failed to update airdrop state, error {:?}", err))
+        })?;
+        AIRDROP_LOGS
+            .with(|r| r.borrow_mut().append(&AirdropLog(user, time, amount)))
+            .map_err(|err| format!("failed to append airdrop log, error {:?}", err))
+    }
+
+    // get airdrop logs in reverse order, return the next index to fetch.
+    pub fn logs(size: usize, idx: Option<u64>) -> (Vec<AirdropLog>, Option<u64>) {
+        AIRDROP_LOGS.with(|r| {
+            let log_store = r.borrow();
+            let latest = log_store.len();
+            if latest == 0 {
+                return (vec![], None);
+            }
+            let mut idx = idx.unwrap_or(latest - 1);
+            if idx >= latest {
+                return (vec![], None);
+            }
+
+            let mut logs = Vec::with_capacity(size);
+            while let Some(log) = log_store.get(idx) {
+                logs.push(log);
+                if idx == 0 {
+                    return (logs, None);
+                }
+
+                if logs.len() >= size {
+                    break;
+                }
+                idx -= 1;
+            }
+
+            (logs, Some(idx))
         })
-        .expect("failed to set STATE data");
+    }
 }
 
-pub fn get_airdrop(user: Principal) -> Option<u64> {
-    AIRDROP.with(|r| r.borrow().get(&user))
+pub mod luckydraw {
+    use super::*;
+
+    pub fn total() -> u64 {
+        LUCKYDRAW_LOGS.with(|r| r.borrow().len())
+    }
+
+    // update luckydraw state and append a log.
+    // return the log index or an error message when append failed.
+    pub fn update(
+        user: Principal,
+        time: u64,
+        token_amount: u64,
+        icp_amount: u64,
+        random: u64,
+    ) -> Result<u64, String> {
+        STATE.with(|r| {
+            let mut borrowed = r.borrow_mut();
+            let mut state = borrowed.get().clone();
+            state.total_luckydraw = state.total_luckydraw.saturating_add(token_amount);
+            state.total_luckydraw_icp = state.total_luckydraw_icp.saturating_add(icp_amount);
+            state.total_luckydraw_count = state.total_luckydraw_count.saturating_add(1);
+            borrowed
+                .set(state)
+                .map_err(|err| format!("failed to update luckydraw state, error {:?}", err))
+        })?;
+        LUCKYDRAW_LOGS
+            .with(|r| {
+                r.borrow_mut()
+                    .append(&LuckyDrawLog(user, time, token_amount, icp_amount, random))
+            })
+            .map_err(|err| format!("failed to append luckydraw log, error {:?}", err))
+    }
+
+    // get luckydraw logs in reverse order, return the next index to fetch.
+    pub fn logs(size: usize, idx: Option<u64>) -> (Vec<LuckyDrawLog>, Option<u64>) {
+        LUCKYDRAW_LOGS.with(|r| {
+            let log_store = r.borrow();
+            let latest = log_store.len();
+            if latest == 0 {
+                return (vec![], None);
+            }
+            let mut idx = idx.unwrap_or(latest - 1);
+            if idx >= latest {
+                return (vec![], None);
+            }
+
+            let mut logs = Vec::with_capacity(size);
+            while let Some(log) = log_store.get(idx) {
+                logs.push(log);
+                if idx == 0 {
+                    return (logs, None);
+                }
+
+                if logs.len() >= size {
+                    break;
+                }
+                idx -= 1;
+            }
+
+            (logs, Some(idx))
+        })
+    }
 }
 
-pub fn set_airdrop(user: Principal, amount: u64) {
-    AIRDROP.with(|r| r.borrow_mut().insert(user, amount));
+pub mod user {
+    use super::*;
+
+    // add a user to the active user set, return true if the user is not in the set
+    pub fn active(user: Principal) -> bool {
+        ACTIVE_USERS.with(|r| r.borrow_mut().insert(user))
+    }
+
+    pub fn deactive(user: Principal) {
+        ACTIVE_USERS.with(|r| r.borrow_mut().remove(&user));
+    }
+}
+
+pub mod state {
+    use super::*;
+
+    pub fn get() -> State {
+        STATE.with(|r| r.borrow().get().clone())
+    }
+
+    // /// A helper function to access the state.
+    // pub fn with<R>(f: impl FnOnce(&State) -> R) -> R {
+    //     STATE.with(|r| f(r.borrow().get()))
+    // }
+
+    // /// A helper function to change the state.
+    // pub fn with_mut(f: impl FnOnce(&mut State)) {
+    //     STATE
+    //         .with(|r| {
+    //             let mut borrowed = r.borrow_mut();
+    //             let mut state = borrowed.get().clone();
+    //             f(&mut state);
+    //             borrowed.set(state)
+    //         })
+    //         .expect("failed to set STATE data");
+    // }
 }
