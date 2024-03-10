f48061334d6601e96f7d10fcbd02c1fd3a874062
0xZensh <txr1883@gmail.com>
2024-03-11 07:26:48 +0800

    A crowd minting formula for ICPanda badges.

--- src/ic_panda_badges/src/lib.rs
+++ src/ic_panda_badges/src/lib.rs
@@ -4,3 +4,21 @@ fn api_version() -> u16 {
 }
 
 ic_cdk::export_candid!();
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn test_crowd_minting_formula() {
+        println!("f = (ln(n) + 1)) / n");
+
+        for n in [
+            1u32, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 100, 200, 500, 1000, 2000, 5000,
+            10000, 50000, 100000, 1000000,
+        ] {
+            let t = (n as f32).ln() + 1f32;
+
+            println!("f({}) -> {:.6}, sum: {:.2}", n, t / n as f32, t);
+        }
+    }
+}
