5ac0c15a81bcb9e3e99467247d5cc0921cfcfd5b
0xZensh <txr1883@gmail.com>
2024-04-11 15:50:50 +0800

    proposals: add 17, 18 to upgrade app

--- proposals/proposal-018.sh
+++ proposals/proposal-018.sh
@@ -3,13 +3,13 @@
 # Load the environment variables
 source "$(pwd)"/proposals/env.sh
 
-export BLOB="$(didc encode --format blob '(record {batch_id=27:nat; evidence=blob "\96\53\c9\de\a7\f0\a8\38\dc\f4\b9\90\38\a8\ef\b2\49\fe\b1\6f\9f\58\18\0d\88\0c\14\c1\ee\9c\81\12"})')"
+export BLOB="$(didc encode --format blob '(record {batch_id=28:nat; evidence=blob "\e8\d5\8c\73\4b\89\97\68\e2\57\8b\a4\28\9d\cf\a8\01\34\13\ee\0f\77\20\cc\0c\35\f9\87\c3\9f\34\f1"})')"
 
 quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
     record {
-        title = \"Execute commit_proposed_batch() to release ic_panda_frontend v1.1.0\";
+        title = \"Execute commit_proposed_batch() to release ic_panda_frontend v1.2.0\";
         url = \"https://panda.fans/\";
-        summary = \"This proposal executes commit_proposed_batch() on c63a7-6yaaa-aaaap-ab3gq-cai to release ic_panda_frontend v1.1.0.\";
+        summary = \"This proposal executes commit_proposed_batch() on c63a7-6yaaa-aaaap-ab3gq-cai to release ic_panda_frontend v1.2.0.\n\n-feat: Give airdrop cryptograms on Lucky Draw.\n\";
         action = opt variant {
             ExecuteGenericNervousSystemFunction = record {
                 function_id = 1000 : nat64;
