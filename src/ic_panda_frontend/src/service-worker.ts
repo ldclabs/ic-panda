7d24e1940fd726b6b6a4d80ba3dba8210e651505
0xZensh <txr1883@gmail.com>
2024-04-22 00:18:49 +0800

    Chore: remove 'registerRoute' in service-worker for some caching issue

--- src/ic_panda_frontend/src/service-worker.ts
+++ src/ic_panda_frontend/src/service-worker.ts
@@ -1,15 +1,11 @@
 /// <reference lib="webworker" />
 
-import { clientsClaim, type WorkboxPlugin } from 'workbox-core'
-import { ExpirationPlugin } from 'workbox-expiration'
+import { clientsClaim } from 'workbox-core'
 import {
   cleanupOutdatedCaches,
-  matchPrecache,
   precacheAndRoute,
   type PrecacheEntry
 } from 'workbox-precaching'
-import { registerRoute, setCatchHandler } from 'workbox-routing'
-import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies'
 
 declare let self: ServiceWorkerGlobalScope
 
@@ -26,51 +22,3 @@ if (indexHtml) {
 }
 precacheAndRoute(precaches)
 cleanupOutdatedCaches()
-
-registerRoute(
-  new RegExp('https://panda\\.fans.*'),
-  new StaleWhileRevalidate({
-    cacheName: 'assets-res',
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 1000,
-        maxAgeSeconds: 7 * 24 * 60 * 60
-      }) as WorkboxPlugin
-    ],
-    matchOptions: {
-      ignoreMethod: false,
-      ignoreSearch: true,
-      ignoreVary: false
-    }
-  })
-)
-
-registerRoute(
-  new RegExp('https://icp-api\\.io/api/.*'),
-  new NetworkFirst({
-    cacheName: 'api-res',
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 10000,
-        maxAgeSeconds: 3 * 24 * 60 * 60
-      }) as WorkboxPlugin
-    ],
-    matchOptions: {
-      ignoreMethod: false,
-      ignoreSearch: false,
-      ignoreVary: false
-    }
-  })
-)
-
-setCatchHandler(async ({ request }) => {
-  let res: Response | undefined
-  switch (request.destination) {
-    case 'document':
-      res = await matchPrecache('index.html')
-    // case 'image':
-    //   return matchPrecache(FALLBACK_IMAGE_URL)
-  }
-
-  return res || Response.error()
-})
