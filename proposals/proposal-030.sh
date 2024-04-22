6584eb3e4137392480130d830fcb426c7946312b
0xZensh <txr1883@gmail.com>
2024-04-22 23:52:44 +0800

    Proposal: add #30 to upgrade app to v1.5.2

---
+++ proposals/proposal-030.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+# build and get batch_id, evidence:
+# dfx deploy ic_panda_frontend --network ic --by-proposal
+
+export BLOB="$(didc encode --format blob '(record {batch_id=35:nat; evidence=blob "\a3\2b\7d\6a\8a\f9\8e\2b\08\2a\10\8e\04\c1\31\e6\6f\e3\5d\92\63\3a\e3\0a\02\3b\46\e2\0c\59\53\3d"})')"
+
+quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
+    record {
+        title = \"Execute commit_proposed_batch() to release ic_panda_frontend v1.5.2\";
+        url = \"https://panda.fans/\";
+        summary = \"This proposal executes commit_proposed_batch() on c63a7-6yaaa-aaaap-ab3gq-cai to release ic_panda_frontend v1.5.2.\n\n1. Chore: improve Prize UI.\";
+        action = opt variant {
+            ExecuteGenericNervousSystemFunction = record {
+                function_id = 1000 : nat64;
+                payload = ${BLOB};
+            }
+        };
+    }
+)" > proposal-message.json
+
+quill send proposal-message.json
