0c498a4656aeb8f4463a42ad87d887ebbf05ec37
0xZensh <txr1883@gmail.com>
2024-06-21 11:19:12 +0800

    proposal: add #72 to upgrade app to v1.5.10

--- proposals/proposal-072.sh
+++ proposals/proposal-072.sh
@@ -6,13 +6,13 @@ source "$(pwd)"/proposals/env.sh
 # build and get batch_id, evidence:
 # dfx deploy ic_panda_frontend --ic --by-proposal
 
-export BLOB="$(didc encode --format blob '(record {batch_id=42:nat; evidence=blob "\02\e0\da\7a\7e\8a\8a\20\74\c7\72\39\4e\37\0a\7e\92\01\91\94\72\19\f3\1d\59\eb\3c\fe\6a\8f\a5\6c"})')"
+export BLOB="$(didc encode --format blob '(record {batch_id=43:nat; evidence=blob "\8c\63\d3\cf\92\d7\48\e7\24\1a\ee\c8\ab\e1\0c\a1\32\10\6b\be\81\8c\4a\a6\e0\2b\c6\67\b4\df\78\7b"})')"
 
 quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
     record {
-        title = \"Execute commit_proposed_batch() to release ic_panda_frontend v1.5.9\";
+        title = \"Execute commit_proposed_batch() to release ic_panda_frontend v1.5.10\";
         url = \"https://panda.fans/\";
-        summary = \"This proposal executes commit_proposed_batch() on c63a7-6yaaa-aaaap-ab3gq-cai to release ic_panda_frontend v1.5.9.\n\n1. Fix: fix app UI style.\";
+        summary = \"This proposal executes commit_proposed_batch() on c63a7-6yaaa-aaaap-ab3gq-cai to release ic_panda_frontend v1.5.10.\n\n1. chore: update roadmap.\";
         action = opt variant {
             ExecuteGenericNervousSystemFunction = record {
                 function_id = 1000 : nat64;
@@ -22,4 +22,4 @@ quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $
     }
 )" > proposal-message.json
 
-# quill send proposal-message.json
+quill send proposal-message.json
