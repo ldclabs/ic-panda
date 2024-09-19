1d94add0f0d29ef838f37b2fd53603f9b9afca71
0xZensh <txr1883@gmail.com>
2024-09-19 11:20:53 +0800

    chore: add proposal to upgrade app

--- proposals/proposal-102.sh
+++ proposals/proposal-102.sh
@@ -6,13 +6,13 @@ source "$(pwd)"/proposals/env.sh
 # build and get batch_id, evidence:
 # dfx deploy ic_panda_frontend --ic --by-proposal
 
-export BLOB="$(didc encode --format blob '(record {batch_id=47:nat; evidence=blob "\23\f0\47\10\d1\8c\86\fd\5a\fa\89\9f\0a\1e\89\09\39\d7\ed\08\aa\57\2a\3c\cd\40\e9\03\51\d2\19\f2"})')"
+export BLOB="$(didc encode --format blob '(record {batch_id=48:nat; evidence=blob "\ae\e7\e3\5d\62\9f\26\ce\55\fa\62\50\81\69\6e\62\3d\93\15\9f\72\95\bd\76\ed\0b\c7\4f\24\fa\da\88"})')"
 
 quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
     record {
-        title = \"Execute commit_proposed_batch() to release ic_panda_frontend v2.1.0\";
+        title = \"Execute commit_proposed_batch() to release ic_panda_frontend v2.1.1\";
         url = \"https://panda.fans/\";
-        summary = \"This proposal executes commit_proposed_batch() on c63a7-6yaaa-aaaap-ab3gq-cai to release ic_panda_frontend v2.1.0.\n\n1. feat: support message deleting; \n2. fix: fix some issues.\";
+        summary = \"This proposal executes commit_proposed_batch() on c63a7-6yaaa-aaaap-ab3gq-cai to release ic_panda_frontend v2.1.1.\n\n1. chore: remove Google reCAPTCHA and GTA; \n2. fix: fix some UI issues.\";
         action = opt variant {
             ExecuteGenericNervousSystemFunction = record {
                 function_id = 1000 : nat64;
