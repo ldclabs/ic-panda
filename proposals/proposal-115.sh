e12fbf4f280fc8adad1f12bfee257cdafd6c4a60
0xZensh <txr1883@gmail.com>
2024-09-25 20:36:16 +0800

    chore: add proposals to upgrade messaging app

---
+++ proposals/proposal-115.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+# build and get batch_id, evidence:
+# dfx deploy ic_panda_frontend --ic --by-proposal
+
+export BLOB="$(didc encode --format blob '(record {batch_id=51:nat; evidence=blob "\82\24\ee\f4\9f\74\64\3d\b3\a5\b0\52\e9\83\f9\76\8d\9b\6d\c8\e1\81\86\8c\d4\60\85\3e\f6\53\f5\9d"})')"
+
+quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
+    record {
+        title = \"Execute commit_proposed_batch() to release ic_panda_frontend v2.2.2\";
+        url = \"https://panda.fans/\";
+        summary = \"This proposal executes commit_proposed_batch() on c63a7-6yaaa-aaaap-ab3gq-cai to release ic_panda_frontend v2.2.2.\n\n1. feat: support transfering username; \n2. chore: update landing page.\";
+        action = opt variant {
+            ExecuteGenericNervousSystemFunction = record {
+                function_id = 1000 : nat64;
+                payload = ${BLOB};
+            }
+        };
+    }
+)" > proposal-message.json
+
+# quill send proposal-message.json
---
+++ proposals/proposal-116.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal '(
+    record {
+        title = "Add a generic function \"admin_update_price\" to messaging management service";
+        url = "https://github.com/ldclabs/ic-panda/blob/main/src/ic_message/src/api_admin.rs#L47";
+        summary = "The `admin_update_price` function is used to update the price for registering a username and creating a channel.";
+        action = opt variant {
+            AddGenericNervousSystemFunction = record {
+                id = 1_104 : nat64;
+                name = "`admin_update_price` function";
+                description = opt "It is used to update the price for registering a username and creating a channel.";
+                function_type = opt variant {
+                    GenericNervousSystemFunction = record {
+                        validator_canister_id = opt principal "nscli-qiaaa-aaaaj-qa4pa-cai";
+                        target_canister_id = opt principal "nscli-qiaaa-aaaaj-qa4pa-cai";
+                        validator_method_name = opt "validate2_admin_update_price";
+                        target_method_name = opt "admin_update_price";
+                    }
+                };
+            }
+        };
+    }
+)' > proposal-message.json
+
+# quill send proposal-message.json
