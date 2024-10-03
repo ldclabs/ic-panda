35f111b049786363fac70f37254cc744dad8969c
0xZensh <txr1883@gmail.com>
2024-10-03 14:01:38 +0800

    chore: add proposal to upgrade frontend

---
+++ proposals/proposal-128.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+# build and get batch_id, evidence:
+# dfx deploy ic_panda_frontend --ic --by-proposal
+
+export BLOB="$(didc encode --format blob '(record {batch_id=55:nat; evidence=blob "\b3\91\c8\e8\e6\ec\1e\ed\ec\0a\65\28\18\67\03\3a\05\4a\99\fb\f7\03\a1\4e\b0\64\fd\55\da\48\11\30"})')"
+
+quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
+    record {
+        title = \"Execute commit_proposed_batch() to release ic_panda_frontend v2.2.6\";
+        url = \"https://panda.fans/\";
+        summary = \"This proposal executes commit_proposed_batch() on c63a7-6yaaa-aaaap-ab3gq-cai to release ic_panda_frontend v2.2.6.\n\n1. fix: fix frontend issues.\";
+        action = opt variant {
+            ExecuteGenericNervousSystemFunction = record {
+                function_id = 1000 : nat64;
+                payload = ${BLOB};
+            }
+        };
+    }
+)" > proposal-message.json
+
+quill send proposal-message.json
