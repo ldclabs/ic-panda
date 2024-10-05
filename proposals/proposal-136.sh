3ee85e5658e0f43dbd398b12c90dfaf1151cbc79
0xZensh <txr1883@gmail.com>
2024-10-05 18:53:17 +0800

    chore: add proposal to upgrade messaging app

---
+++ proposals/proposal-136.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+# build and get batch_id, evidence:
+# dfx deploy ic_message_frontend --ic --by-proposal
+
+export BLOB="$(didc encode --format blob '(record {batch_id=11:nat; evidence=blob "\b6\2d\45\75\2f\51\6b\49\fc\d3\04\88\d9\b8\3e\de\d1\f7\b3\0c\f6\8a\9e\6c\d8\fa\f5\64\c3\e8\db\4d"})')"
+
+quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
+    record {
+        title = \"Execute commit_proposed_batch() to release ic_message_frontend v2.4.0\";
+        url = \"https://panda.fans/\";
+        summary = \"This proposal executes commit_proposed_batch() on 2fvu6-tqaaa-aaaap-akksa-cai to release ic_message_frontend v2.4.0.\n\n1. feat: 1. Support dark mode; \n2. Improve profile page.\";
+        action = opt variant {
+            ExecuteGenericNervousSystemFunction = record {
+                function_id = 1100 : nat64;
+                payload = ${BLOB};
+            }
+        };
+    }
+)" > proposal-message.json
+
+# quill send proposal-message.json
