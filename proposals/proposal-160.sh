0313f8c6e5a1b2fc9f89121c9cd90b3be8e2bdb3
0xZensh <txr1883@gmail.com>
2024-10-14 08:39:51 +0800

    chore: add proposal to upgrade messaging app

---
+++ proposals/proposal-160.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+# build and get batch_id, evidence:
+# dfx deploy ic_message_frontend --ic --by-proposal
+
+export BLOB="$(didc encode --format blob '(record {batch_id=20:nat; evidence=blob "\34\47\22\6b\14\4c\70\8b\61\06\7a\7e\ad\4d\6e\7e\98\24\47\38\c5\a8\05\e0\26\b8\7d\04\91\2b\70\c7"})')"
+
+quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
+    record {
+        title = \"Execute commit_proposed_batch() to release ic_message_frontend v2.5.5\";
+        url = \"https://panda.fans/\";
+        summary = \"This proposal executes commit_proposed_batch() on 2fvu6-tqaaa-aaaap-akksa-cai to release ic_message_frontend v2.5.5.\n\n1. feat: support QR in profile page.\n2. fix: fix profile link UI.\";
+        action = opt variant {
+            ExecuteGenericNervousSystemFunction = record {
+                function_id = 1100 : nat64;
+                payload = ${BLOB};
+            }
+        };
+    }
+)" > proposal-message.json
+
+# quill send proposal-message.json
