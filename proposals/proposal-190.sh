aca2a27b70e01659eeb51cb9d91fb13d61b19843
0xZensh <txr1883@gmail.com>
2024-10-25 18:16:23 +0800

    chore: add proposals to upgrade fontend

---
+++ proposals/proposal-190.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+# build and get batch_id, evidence:
+# dfx deploy ic_panda_frontend --ic --by-proposal
+
+export BLOB="$(didc encode --format blob '(record {batch_id=60:nat; evidence=blob "\6c\02\d0\91\8f\ca\d5\26\ab\f9\76\20\50\be\8b\23\0b\22\5e\03\78\29\70\01\1c\15\43\64\87\2b\1a\69"})')"
+
+quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
+    record {
+        title = \"Execute commit_proposed_batch() to release ic_panda_frontend v2.6.2\";
+        url = \"https://panda.fans/\";
+        summary = \"This proposal executes commit_proposed_batch() on c63a7-6yaaa-aaaap-ab3gq-cai to release ic_panda_frontend v2.6.2.\n\n1. chore: remove legacy messaging app in 'ic_panda_front'.\";
+        action = opt variant {
+            ExecuteGenericNervousSystemFunction = record {
+                function_id = 1000 : nat64;
+                payload = ${BLOB};
+            }
+        };
+    }
+)" > proposal-message.json
+
+# quill send proposal-message.json
---
+++ proposals/proposal-191.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+# build and get batch_id, evidence:
+# dfx deploy ic_message_frontend --ic --by-proposal
+
+export BLOB="$(didc encode --format blob '(record {batch_id=26:nat; evidence=blob "\20\3b\a9\fd\e6\18\01\75\2a\b1\84\08\71\55\93\4a\54\e4\30\6b\c0\cb\23\e5\18\ab\b6\98\2e\35\60\e2"})')"
+
+quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
+    record {
+        title = \"Execute commit_proposed_batch() to release ic_message_frontend v2.6.2\";
+        url = \"https://dMsg.net/\";
+        summary = \"This proposal executes commit_proposed_batch() on 2fvu6-tqaaa-aaaap-akksa-cai to release ic_message_frontend v2.6.2.\n\n1. chore: update dependencies.\n2. chore: update partners.\";
+        action = opt variant {
+            ExecuteGenericNervousSystemFunction = record {
+                function_id = 1100 : nat64;
+                payload = ${BLOB};
+            }
+        };
+    }
+)" > proposal-message.json
+
+# quill send proposal-message.json
