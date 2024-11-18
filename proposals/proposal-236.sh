1c26f732a62f0773c3200d03e9fe1c3b1d17e486
0xZensh <txr1883@gmail.com>
2024-11-18 11:32:12 +0800

    chore: update proposals

---
+++ proposals/proposal-236.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+# build and get batch_id, evidence:
+# dfx deploy ic_message_frontend --ic --by-proposal
+
+export BLOB="$(didc encode --format blob '(record {batch_id=32:nat; evidence=blob "\9d\1d\73\5e\63\06\ac\c6\7b\7c\83\39\bb\7b\9a\00\2c\e7\8e\b9\d6\51\98\17\dc\e0\93\3f\ec\08\f9\a7"})')"
+
+quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
+    record {
+        title = \"Execute commit_proposed_batch() to release ic_message_frontend v2.6.9\";
+        url = \"https://dMsg.net/\";
+        summary = \"This proposal executes commit_proposed_batch() on 2fvu6-tqaaa-aaaap-akksa-cai to release ic_message_frontend v2.6.9.\n\n1. fix: fix UX issues.\";
+        action = opt variant {
+            ExecuteGenericNervousSystemFunction = record {
+                function_id = 1100 : nat64;
+                payload = ${BLOB};
+            }
+        };
+    }
+)" > proposal-message.json
+
+quill send proposal-message.json
