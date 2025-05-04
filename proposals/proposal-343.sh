6839aae99b8fd5b11a87fd404b2f3b43855f4857
0xZensh <txr1883@gmail.com>
2025-05-04 21:44:37 +0800

    chore: add proposals to upgrade frontend

---
+++ proposals/proposal-343.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+# build and get batch_id, evidence:
+# dfx deploy ic_panda_frontend --ic --by-proposal
+
+export BLOB="$(didc encode --format blob '(record {batch_id=68:nat; evidence=blob "\9c\10\f7\ac\e6\e5\81\8c\33\8b\5f\5c\aa\45\99\f1\6c\ea\26\39\3c\1b\92\8d\ce\3e\35\97\c7\09\35\e3"})')"
+
+quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
+    record {
+        title = \"Execute commit_proposed_batch() to release ic_panda_frontend v2.10.0\";
+        url = \"https://panda.fans/\";
+        summary = \"This proposal executes commit_proposed_batch() on c63a7-6yaaa-aaaap-ab3gq-cai to release ic_panda_frontend v2.10.0.\n\n1. chore: update dependencies.\";
+        action = opt variant {
+            ExecuteGenericNervousSystemFunction = record {
+                function_id = 1000 : nat64;
+                payload = ${BLOB};
+            }
+        };
+    }
+)" > proposal-message.json
+
+# quill send proposal-message.json
---
+++ proposals/proposal-344.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+# build and get batch_id, evidence:
+# dfx deploy ic_message_frontend --ic --by-proposal
+
+export BLOB="$(didc encode --format blob '(record {batch_id=54:nat; evidence=blob "\02\0a\76\d8\cd\0a\85\a6\94\38\49\30\68\61\47\ea\50\17\29\1f\ac\e7\aa\8c\ba\6e\e7\21\d0\ee\96\1e"})')"
+
+quill sns make-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE $PROPOSAL_NEURON_ID --proposal "(
+    record {
+        title = \"Execute commit_proposed_batch() to release ic_message_frontend v2.10.0\";
+        url = \"https://dMsg.net/\";
+        summary = \"This proposal executes commit_proposed_batch() on 2fvu6-tqaaa-aaaap-akksa-cai to release ic_message_frontend v2.10.0.\n\n1. chore: update dependencies.\";
+        action = opt variant {
+            ExecuteGenericNervousSystemFunction = record {
+                function_id = 1100 : nat64;
+                payload = ${BLOB};
+            }
+        };
+    }
+)" > proposal-message.json
+
+# quill send proposal-message.json
