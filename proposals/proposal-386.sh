26bb083685bc54a4951b2f740b345841aef06be0
0xZensh <txr1883@gmail.com>
2025-06-30 11:16:58 +0800

    chore: add proposals to upgrade $DMSG canisters

---
+++ proposals/proposal-386.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+export CANISTERS_PATH="$(pwd)/.dfx/ic/github"
+
+# https://forum.dfinity.org/t/icrc-ledger-suite-upgrade/40655
+
+quill sns make-upgrade-canister-proposal $PROPOSAL_NEURON_ID --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE --target-canister-id "ocqzv-tyaaa-aaaar-qal4a-cai" --wasm-path "$CANISTERS_PATH/ic-icrc1-ledger.wasm.gz" --mode upgrade --title "Upgrade dmsg_ledger_canister to ledger-suite-icrc-2025-04-14" --summary "upgrade: ledger-suite-icrc-2025-04-14" --url "https://github.com/dfinity/ic/releases/tag/ledger-suite-icrc-2025-04-14" > proposal-message.json
+
+# quill send proposal-message.json
---
+++ proposals/proposal-387.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+export CANISTERS_PATH="$(pwd)/.dfx/ic/github"
+
+# https://forum.dfinity.org/t/icrc-ledger-suite-upgrade/40655
+
+quill sns make-upgrade-canister-proposal $PROPOSAL_NEURON_ID --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE --target-canister-id "qc6wh-6yaaa-aaaap-anuza-cai" --wasm-path "$CANISTERS_PATH/ic-icrc1-index-ng.wasm.gz" --mode upgrade --title "Upgrade dmsg_index_canister to ledger-suite-icrc-2025-06-10" --summary "upgrade: ledger-suite-icrc-2025-06-10" --url "https://github.com/dfinity/ic/releases/tag/ledger-suite-icrc-2025-06-10" > proposal-message.json
+
+# quill send proposal-message.json
