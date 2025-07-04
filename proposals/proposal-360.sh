397811a5748402242bd9f41bcdc3a224e176a11b
0xZensh <txr1883@gmail.com>
2025-07-04 14:43:16 +0800

    chore: add proposals to upgrade IC OSS service

--- proposals/proposal-360.sh
+++ proposals/proposal-360.sh
@@ -7,4 +7,4 @@ export CANISTERS_PATH="$(pwd)/.dfx/ic/github"
 
 quill sns make-upgrade-canister-proposal $PROPOSAL_NEURON_ID --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE --target-canister-id "5szpn-tiaaa-aaaaj-qncoq-cai" --wasm-path "$CANISTERS_PATH/ic_oss_cluster.wasm.gz" --mode upgrade --title "Upgrade ic_oss_cluster canister to v1.1.3" --summary "chore: update dependencies" --url "https://github.com/ldclabs/ic-oss/releases/tag/v1.1.3" > proposal-message.json
 
-quill send proposal-message.json
+# quill send proposal-message.json
