350a82a3636f6e3ba725758c4a5ba3698e6ec94c
0xZensh <txr1883@gmail.com>
2025-07-30 08:36:11 +0800

    chore: add proposals to upgrade IC-COSE and IC-OSS

---
+++ proposals/proposal-404.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+export CANISTERS_PATH="$(pwd)/.dfx/ic/github"
+
+quill sns make-upgrade-canister-proposal $PROPOSAL_NEURON_ID --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE --target-canister-id "n3bau-gaaaa-aaaaj-qa4oq-cai" --wasm-path "$CANISTERS_PATH/ic_cose_canister.wasm.gz" --mode upgrade --title "Upgrade ic_cose_canister canister to v0.8.12" --summary "chore: update dependencies" --url "https://github.com/ldclabs/ic-cose/releases/tag/v0.8.12" > proposal-message.json
+
+# quill send proposal-message.json
