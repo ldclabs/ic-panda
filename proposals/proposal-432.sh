8e542b718c5cc44b0554f7e164c3f5b528defcb9
0xZensh <txr1883@gmail.com>
2025-09-26 20:01:48 +0800

    chore: add proposals

---
+++ proposals/proposal-432.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+export CANISTERS_PATH="$(pwd)/.dfx/ic/github"
+
+quill sns make-upgrade-canister-proposal --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE --target-canister-id "nvdn4-5qaaa-aaaaj-qa4pq-cai" --wasm-path "$CANISTERS_PATH/ic_message_channel.wasm.gz" --mode upgrade --title "Upgrade ic_message_channel canister to v2.13.0" --summary "chore: update dependencies" --url "https://github.com/ldclabs/ic-panda/releases/tag/v2.13.0" $PROPOSAL_NEURON_ID > proposal-message.json
+
+# quill send proposal-message.json
