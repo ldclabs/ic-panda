2cfaecc9d3747646ac52499b3873710e2fcc37c5
0xZensh <txr1883@gmail.com>
2025-10-15 20:33:44 +0800

    chore: add proposals to upgrade IC OSS canisters

---
+++ proposals/proposal-442.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Load the environment variables
+source "$(pwd)"/proposals/env.sh
+
+export CANISTERS_PATH="$(pwd)/.dfx/ic/github"
+
+quill sns make-upgrade-canister-proposal $PROPOSAL_NEURON_ID --canister-ids-file ./sns_canister_ids.json --pem-file $PROPOSAL_PEM_FILE --target-canister-id "5szpn-tiaaa-aaaaj-qncoq-cai" --wasm-path "$CANISTERS_PATH/ic_oss_cluster.wasm.gz" --mode upgrade --title "Upgrade ic_oss_cluster canister to v1.2.2" --summary "chore: update with latest ICP crates" --url "https://github.com/ldclabs/ic-oss/releases/tag/v1.2.2" > proposal-message.json
+
+# quill send proposal-message.json
